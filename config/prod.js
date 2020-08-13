91ebdc8b2eb91f61a2a903dd4e6523cb63525f6d
haoranyu <hy47@cs.washington.edu>
2020-08-12 20:37:29 -0700

    changed

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: 'mongodb+srv://myuser:yhr990702@cluster0.35azq.mongodb.net/emaily?retryWrites=true&w=majority',
+    mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
